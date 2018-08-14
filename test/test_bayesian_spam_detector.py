0830d5f23b7d4e4019e6a2b9e5bee1867a9a8586
Michael Kane <michael.kane@flatironschool.com>
2018-08-13 22:38:13 -0700

    begin adding testing examples

--- test/test_bayesian_spam_detector.py
+++ test/test_bayesian_spam_detector.py
@@ -1,11 +1,40 @@
 import pytest
+import pandas
+# import matplotlib
+# import seaborn
+import sklearn
 # import ipython
-from ipynb.fs.full.index import (df, column_names, target, clean_df, X_train,
-X_test, y_train, y_test, accuracy, f1, confusion_matrix, training_preds,
-training_cm, testing_cm)
+from ipynb.fs.full.index import *
+
+def test_p1_training_cm():
+    pass
+
+def test_p2_training_cm():
+    pass
+
+def test_p3_training_cm():
+    pass
+
+def test_p4_training_cm():
+    pass
+
+def test_p5_training_cm():
+    pass
+
+def test_p6_training_cm():
+    pass
+
+def test_p7_training_cm():
+    pass
+
+def test_p8_training_cm():
+    pass
 
 def test_p9_training_cm():
     assert training_cm["TP"] == 1310
     assert training_cm["TN"] == 1524
     assert training_cm["FP"] == 567
     assert training_cm["FN"] == 49
+
+def test_p10_training_cm():
+    pass
